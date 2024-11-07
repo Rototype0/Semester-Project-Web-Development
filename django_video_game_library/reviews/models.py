9c5c5af53e3930777497b5f604192d1114a8d5bc
Drago361 <alexftvyt@gmail.com>
2024-11-07 16:50:43 +0100

    Added Reviews
    Added reviews and rating system with five stars for now, Future integration for the Steam appid games is needed, and will be done. Future integration with steam reviews will be added and user login system is needed for the reviews to be added fully and working with users assigned to them

--- django_video_game_library/reviews/models.py
+++ django_video_game_library/reviews/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+#from games_list.models import Game
+from django.db.models import Avg
 
 # Create your models here.
 
 class Review(models.Model):
+    header = models.CharField(max_length=100, default="Header")
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    #author = models.ForeignKey(User, on_delete = models.CASCADE)
+    #rating = models.IntegerField(default = 0)
+    #appid = models.ForeignKey(Game.appid, on_delete = models.CASCADE)
+
+    #def __str__(self):
+    #    return f"{self.post.header}: {self.rating}"
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    review = models.ForeignKey(Review, on_delete = models.CASCADE)
+    #appid = models.ForeignKey(Game.appid, on_delete = models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.post.header}: {self.rating}"
+
--- django_video_game_library/reviews/views.py
+++ django_video_game_library/reviews/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render
-from .models import Review
-# Create your views here.
-
-reviews = [
-    {
-        'author': 'Hristo',
-        'content': 'This game sucks >:(',
-        'date_posted': '9/20/2024',
-    },
-    {
-        'author': 'NotHristo',
-        'content': 'I love this game :D',
-        'date_posted': '9/20/2024',
-    },
-]
-
+from .models import Review, Rating
 def Demo(request):
     return render(request, 'reviews/demo.html')
 
 def Home(request):
-    context = {
-        'reviews': Review.objects.all(),
-        'title': 'Reviews',
-    }
-    return render(request, 'reviews/home.html', context)
+    reviews = Review.objects.all()
+    for review in reviews:
+        rating = Rating.objects.filter(review=review, user=request.user).first()
+        review.user_rating = rating.rating if rating else 0
+    return render(request, 'reviews/home.html', {"reviews": reviews})
+
+
+def rate(request, post_id: int, rating: int):
+    review = Review.objects.get(id=review_id)
+    Rating.objects.filter(review=review, user=request.user).delete()
+    review.rating_set.create(user=request.user, rating=rating)
+    return index(request)
+
 
 def About(request):
     context = {
         'title': 'About',
     }
     return render(request, 'reviews/about.html', context)
+
+"""def index(request)
+    reviews = Review.objects.all
+    for post in posts:
+        rating = Rating.objects.filter(post=post, user=request.user).first()
+        post.user_rating = rating.rating if rating else 0
+    return render(request, "index.html", {"posts": posts})
+"""
+'''context = {
+        'reviews': Review.objects.all(),
+        'title': 'Reviews',
+    }
+    return render(request, 'reviews/home.html', context)'''
