3fcf1b9da06839524745a293cfc077587e0d4fab
Ivan <newgenverba@gmail.com>
2024-12-04 20:53:20 +0200

    figuring out how to go about the upvotes
    messed around in the models

--- django_video_game_library/reviews/models.py
+++ django_video_game_library/reviews/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 #from games_list.models import Game
 
 # Create your models here.
@@ -55,6 +58,7 @@ class Review(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name='likes') #added likes to review class
     #rating = models.IntegerField(default = 0)
     #appid = models.ForeignKey(Game.appid, on_delete = models.CASCADE)
     #user = models.ForeignKey(Rating.user, on_delete = models.CASCADE)
@@ -79,3 +83,5 @@ class AppID(models.Model):
         return f"{self.appid}"
      '''
 
+#def Upvotes(request, pk):
+    #post = get_object_or_404
